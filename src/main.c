93a9cd83b6651018d7666c7d5689a894c7c3a8e0
YesntDabnt <elalakalaeimai@gmail.com>
2026-04-11 19:42:48 +0200

    make some platforms and player movement and a "hitbox" for the platforms and player

--- src/main.c
+++ src/main.c
@@ -1,88 +1,84 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <SDL.h>
+#include <SDL_image.h>
 #include "player.h"
 #include "map.h"
-
 
 #define WINDOW_WIDTH 1280
 #define WINDOW_HEIGHT 960
 
-
-typedef struct{
+typedef struct {
     SDL_Window *pWindow;
     SDL_Renderer *pRenderer;
     Player *pPlayer;
-    Platform *pPlatform;
+    Platform *pPlatforms;
+    int platformCount;
 } Game;
-
 
 int initiate(Game *pGame);
 void run(Game *pGame);
-void close(Game *pGame);
+void closeGame(Game *pGame);
 
-
-int main(int argv, char** args){
+int main(int argc, char **argv)
+{
     Game game = {0};
-    initiate(&game);
-    run(&game);
-    close(&game);
 
-    /*bool isRunning = true;
-    SDL_Event event;
-
-    while(isRunning){
-        while(SDL_PollEvent(&event)){
-            switch (event.type)
-            {
-                case SDL_QUIT: isRunning = false;
-            }
-        }
-        SDL_RenderClear(game.pRenderer);
-        SDL_SetRenderDrawColor(game.pRenderer, 255, 0, 0, 255);
-        drawPlayer(game.pPlayer);
-        SDL_RenderPresent(game.pRenderer);
-        SDL_Delay(1000/60-15);
+    if (!initiate(&game)) {
+        return 1;
     }
-    SDL_DestroyRenderer(game.pRenderer);
-    SDL_DestroyWindow(game.pWindow);
-    SDL_Quit();*/
 
+    run(&game);
+    closeGame(&game);
     return 0;
 }
 
 int initiate(Game *pGame)
 {
-    if(SDL_Init(SDL_INIT_EVERYTHING)!=0){
-        printf("Error: %s\n",SDL_GetError());
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        printf("SDL Init Error: %s\n", SDL_GetError());
         return 0;
     }
 
-    pGame->pWindow = SDL_CreateWindow("Tank Turtles", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, 0);
-    if(!pGame->pWindow){
-        printf("Error: %s\n",SDL_GetError());
-        close(pGame);
+    if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) {
+        printf("SDL_image Init Error: %s\n", IMG_GetError());
+        SDL_Quit();
         return 0;
     }
 
-    pGame->pRenderer = SDL_CreateRenderer(pGame->pWindow, -1, 0);
-    if(!pGame->pRenderer){
-        printf("Error: %s\n",SDL_GetError());
-        close(pGame);
-        return 0;    
-    }
+    pGame->pWindow = SDL_CreateWindow(
+        "Tank Turtles",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        WINDOW_WIDTH,
+        WINDOW_HEIGHT,
+        0
+    );
 
-    pGame->pPlayer = createPlayer(WINDOW_WIDTH/2, WINDOW_HEIGHT/2, pGame->pRenderer, WINDOW_WIDTH, WINDOW_HEIGHT);
-    if(!pGame->pPlayer){
-        printf("Error: %s\n",SDL_GetError());
-        close(pGame);
+    if (!pGame->pWindow) {
+        printf("Window Error: %s\n", SDL_GetError());
+        closeGame(pGame);
         return 0;
     }
 
-    pGame->pPlatform = createPlatform(pGame->pRenderer,WINDOW_WIDTH,WINDOW_HEIGHT);
-    if(!pGame->pPlatform){
-        printf("Error: %s\n",SDL_GetError());
-        close(pGame);
+    pGame->pRenderer = SDL_CreateRenderer(pGame->pWindow, -1, SDL_RENDERER_ACCELERATED);
+    if (!pGame->pRenderer) {
+        printf("Renderer Error: %s\n", SDL_GetError());
+        closeGame(pGame);
+        return 0;
+    }
+
+    pGame->pPlayer = createPlayer(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, pGame->pRenderer, WINDOW_WIDTH, WINDOW_HEIGHT);
+    if (!pGame->pPlayer) {
+        printf("Player creation failed\n");
+        closeGame(pGame);
+        return 0;
+    }
+
+    pGame->pPlatforms = createPlatforms(pGame->pRenderer, WINDOW_WIDTH, WINDOW_HEIGHT, &pGame->platformCount);
+    if (!pGame->pPlatforms) {
+        printf("Platform creation failed\n");
+        closeGame(pGame);
         return 0;
     }
 
@@ -93,29 +89,35 @@ void run(Game *pGame)
 {
     int close_requested = 0;
     SDL_Event event;
-    while(!close_requested)
-    {
-        while(SDL_PollEvent(&event))
-        {
-            if(event.type==SDL_QUIT) close_requested = 1;
-            //else handleInput(pGame,&event);
-        }
-        
 
-        SDL_RenderClear(pGame->pRenderer);
+    while (!close_requested) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                close_requested = 1;
+            }
+        }
+
+        const Uint8 *keystate = SDL_GetKeyboardState(NULL);
+        updatePlayer(pGame->pPlayer, keystate, pGame->pPlatforms, pGame->platformCount);
+
         SDL_SetRenderDrawColor(pGame->pRenderer, 70, 70, 70, 255);
+        SDL_RenderClear(pGame->pRenderer);
+
+        drawPlatforms(pGame->pPlatforms, pGame->platformCount);
         drawPlayer(pGame->pPlayer);
-        drawPlatform(pGame->pPlatform);
+
         SDL_RenderPresent(pGame->pRenderer);
-        SDL_Delay(1000/60-15);
+        SDL_Delay(16);
     }
 }
 
-void close(Game *pGame)
+void closeGame(Game *pGame)
 {
-    if(pGame->pPlayer) destroyPlayer(pGame->pPlayer);
-    if(pGame->pRenderer) SDL_DestroyRenderer(pGame->pRenderer);
-    if(pGame->pWindow) SDL_DestroyWindow(pGame->pWindow);
-    if(pGame->pPlatform) destroyPlatform(pGame->pPlatform);
+    if (pGame->pPlayer) destroyPlayer(pGame->pPlayer);
+    if (pGame->pPlatforms) destroyPlatforms(pGame->pPlatforms, pGame->platformCount);
+    if (pGame->pRenderer) SDL_DestroyRenderer(pGame->pRenderer);
+    if (pGame->pWindow) SDL_DestroyWindow(pGame->pWindow);
+
+    IMG_Quit();
     SDL_Quit();
 }
--- src/map.c
+++ src/map.c
@@ -1,60 +1,92 @@
 #include <SDL.h>
 #include <SDL_image.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include "map.h"
 
-
-struct platform{ 
-    int window_width,window_height;
+struct platform {
     SDL_Renderer *pRenderer;
     SDL_Texture *pTexture;
     SDL_Rect rect;
+    int active;
 };
 
-
-Platform *createPlatform(SDL_Renderer *pRenderer, int window_width, int window_height)
+Platform *createPlatforms(SDL_Renderer *pRenderer, int window_width, int window_height, int *platformCount)
 {
-    Platform *pPlatform = malloc(sizeof(struct platform));
+    *platformCount = 4;
+
+    Platform *platforms = malloc(sizeof(Platform) * (*platformCount));
+    if (!platforms) return NULL;
+
     SDL_Surface *surface = IMG_Load("Resources/PrimitivPlatform.png");
-    if(!surface)
-    {
-        printf("Error: %s\n",SDL_GetError());
+    if (!surface) {
+        printf("Error loading PrimitivPlatform.png: %s\n", IMG_GetError());
+        free(platforms);
         return NULL;
     }
 
-    pPlatform->pRenderer = pRenderer;
-    pPlatform->pTexture = SDL_CreateTextureFromSurface(pRenderer, surface);
+    SDL_Texture *sharedTexture = SDL_CreateTextureFromSurface(pRenderer, surface);
     SDL_FreeSurface(surface);
-    if(!pPlatform->pTexture)
-    {
-        printf("Error: %s\n",SDL_GetError());
+
+    if (!sharedTexture) {
+        printf("Error creating platform texture: %s\n", SDL_GetError());
+        free(platforms);
         return NULL;
     }
-    
-    
-    pPlatform->window_width = window_width;                           
-    pPlatform->window_height = window_height;       
 
-    SDL_QueryTexture(pPlatform->pTexture,NULL,NULL,&(pPlatform->rect.w),&(pPlatform->rect.h));
+    for (int i = 0; i < *platformCount; i++) {
+        platforms[i].pRenderer = pRenderer;
+        platforms[i].pTexture = sharedTexture;
+        platforms[i].active = 1;
+    }
 
-    
-    pPlatform->rect.w = pPlatform->window_width/2;                         
-    pPlatform->rect.h = pPlatform->window_height/16;                    // size of platform
+    platforms[0].rect.x = 120;
+    platforms[0].rect.y = window_height - 180;
+    platforms[0].rect.w = 350;
+    platforms[0].rect.h = 40;
 
-    pPlatform->rect.x = pPlatform->window_width - pPlatform->window_width/2 - 200;            // location / make a dynamic function that makes the platform apper in the middle of the screen
-    pPlatform->rect.y = pPlatform->window_height - pPlatform->window_height/4;
-    //getStartValues(pPlatform);
-    
+    platforms[1].rect.x = 520;
+    platforms[1].rect.y = window_height - 300;
+    platforms[1].rect.w = 250;
+    platforms[1].rect.h = 40;
 
-    return pPlatform;
+    platforms[2].rect.x = 850;
+    platforms[2].rect.y = window_height - 430;
+    platforms[2].rect.w = 220;
+    platforms[2].rect.h = 40;
+
+    platforms[3].rect.x = 300;
+    platforms[3].rect.y = window_height - 520;
+    platforms[3].rect.w = 200;
+    platforms[3].rect.h = 40;
+
+    return platforms;
 }
 
-void drawPlatform(Platform *pPlatform){
-    SDL_RenderCopy(pPlatform->pRenderer,pPlatform->pTexture,NULL,&(pPlatform->rect));
-}
-
-void destroyPlatform(Platform *pflatform)
+void drawPlatforms(Platform *platforms, int platformCount)
 {
-    free(pflatform);
+    for (int i = 0; i < platformCount; i++) {
+        if (platforms[i].active) {
+            SDL_RenderCopy(platforms[i].pRenderer, platforms[i].pTexture, NULL, &platforms[i].rect);
+
+            SDL_SetRenderDrawColor(platforms[i].pRenderer, 0, 255, 0, 255);
+            SDL_RenderDrawRect(platforms[i].pRenderer, &platforms[i].rect);
+        }
+    }
 }
 
+SDL_Rect getPlatformRect(Platform *platforms, int index)
+{
+    return platforms[index].rect;
+}
+
+void destroyPlatforms(Platform *platforms, int platformCount)
+{
+    if (!platforms) return;
+
+    if (platformCount > 0 && platforms[0].pTexture) {
+        SDL_DestroyTexture(platforms[0].pTexture);
+    }
+
+    free(platforms);
+}
