9dfd26dad4679b2cec9a246ad2d27fc03849cc06
Hampus <Hampuswallinder321@gmail.com>
2026-04-15 10:25:36 +0200

    Changed player png

--- src/player.c
+++ src/player.c
@@ -50,13 +50,13 @@ Player *createPlayer(float x, float y, SDL_Renderer *pRenderer, int window_width
 
     pPlayer->velX = 0.0f;
     pPlayer->velY = 0.0f;
-    pPlayer->moveSpeed = 1.2f;
-    pPlayer->jumpForce = 14.0f;
+    pPlayer->moveSpeed = 5.0f;
+    pPlayer->jumpForce = 18.0f;
     pPlayer->gravity = 0.7f;
     pPlayer->isGrounded = 0;
     pPlayer->isTouchingWall = 0;
 
-    SDL_Surface *pSurface = IMG_Load("Resources/tank.png");
+    SDL_Surface *pSurface = IMG_Load("Resources/firsttank.png");
     if (!pSurface) {
         printf("Error loading tank.png: %s\n", IMG_GetError());
         free(pPlayer);
