5d3f6791b9d85cc503142c510330758a6ef8a34a
ShadowStorm005 <shadowstorm@gmx.com>
2026-04-20 12:06:16 +0200

    changed movement speed

--- src/player.c
+++ src/player.c
@@ -52,8 +52,8 @@ Player *createPlayer(float x, float y, SDL_Renderer *pRenderer, int window_width
 
     pPlayer->velX = 0.0f;
     pPlayer->velY = 0.0f;
-    pPlayer->moveSpeed = 5.0f;
-    pPlayer->jumpForce = 18.0f;
+    pPlayer->moveSpeed = 3.0f;
+    pPlayer->jumpForce = 16.0f;
     pPlayer->gravity = 0.7f;
     pPlayer->isGrounded = 0;
     pPlayer->isTouchingWall = 0;
