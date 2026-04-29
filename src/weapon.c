05ca8bbfd62fc172e80ac9b53a8a178c80259d02
Samuel <samuel.avatare@gmail.com>
2026-04-29 15:30:52 +0200

    Added a dotted trajectory line to tank

--- src/weapon.c
+++ src/weapon.c
@@ -36,7 +36,6 @@ Projectile *createProjectile(SDL_Renderer *pRenderer)
     pProjectile->projectile_rect.x = (int)pProjectile->x;
     pProjectile->projectile_rect.y = (int)pProjectile->y;
     pProjectile->active = 0;
-    pProjectile->gravity = 0.15f;
 
     SDL_Surface *pSurface = IMG_Load("Resources/bullet.png");
     if (!pSurface) {
@@ -94,7 +93,7 @@ void updateProjectileRect(Projectile *pProjectile)
 
 void updateProjectile(Projectile *pProjectile, Map *pMap, NetTile tileChanges[MAX_TILE_CHANGES], uint8_t *tileChangeCount, Sounds *sounds)
 {
-    pProjectile->velY += pProjectile->gravity;
+    pProjectile->velY += PROJECTILE_GRAVITY;
 
     pProjectile->x += pProjectile->velX;
     pProjectile->y += pProjectile->velY;
@@ -134,7 +133,6 @@ void shoot(Projectile *pProjectile[], int size, float speed, float x, float y, f
     {
         if(!pProjectile[i]->active)
         {
-            //bullet size
             pProjectile[i]->projectile_rect.w = size*BULLET_ASPECT;
             pProjectile[i]->projectile_rect.h = size;
 
