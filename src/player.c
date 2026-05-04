f6dae32be632d0a217cd5ea9cf7adcf9c93b4f29
Samuel Avatare <samuel.avatare@gmail.com>
2026-05-04 10:45:45 +0200

    Revert "Trajectories"

--- src/player.c
+++ src/player.c
@@ -25,8 +25,8 @@ struct player {
 
     int window_width, window_height;
 
-    double canonAngle;
-    double targetAngle;
+    float canonAngle;
+    float targetAngle;
 
     SDL_Texture *pHullTx;
     SDL_Texture *pCanonTx;
@@ -231,100 +231,6 @@ int canShoot(Player *pPlayer)
     return pPlayer->canFire;
 }
 
-float getBulletSpeed(Player *pPlayer)
-{
-    switch(pPlayer->canonMode)
-    {
-    case 1:
-        return 16.0f;
-        break;
-    case 2:
-        return 20.0f;
-        break;
-    default:
-        return 16.0f;
-        break;
-    }
-}
-
-int getBulletSize(Player *pPlayer)
-{
-    switch(pPlayer->canonMode)
-    {
-    case 1:
-        return 30;
-        break;
-    case 2:
-        return 15;
-        break;
-    default:
-        return 30;
-        break;
-    }
-}
-
-void drawCircle(SDL_Renderer * renderer, int centerX, int centerY, float rad, int opacity)
-{
-    int outline = 70;
-    int fill = 255;
-    for(int x = -rad; x <= rad; x++)
-    {
-        for(int y = -rad; y <= rad; y++)
-        {
-            float dist = x*x + y*y;
-            float rad2 = rad * rad;
-            if(fabsf(dist - rad2) < rad) 
-            {
-                SDL_SetRenderDrawColor(renderer, outline, outline, outline, opacity);
-                SDL_RenderDrawPoint(renderer, centerX + x, centerY + y);
-            }
-            else if(dist <= rad2) 
-            {
-                SDL_SetRenderDrawColor(renderer, fill, fill, fill, opacity);
-                SDL_RenderDrawPoint(renderer, centerX + x, centerY + y);
-            }
-        }
-    }
-}
-
-void drawTrajectory(Player *pPlayer, float initialSpeed, Map *pMap)
-{
-    if(pPlayer->velX || pPlayer->velX) return;
-    
-    float x = getCanonX(pPlayer);
-    float y = getCanonY(pPlayer);
-
-    float vx = initialSpeed * cos(pPlayer->canonAngle);
-    float vy = initialSpeed * sin(pPlayer->canonAngle);
-    float dt = 0.1f;
-    float rad = 3.0f;
-
-    for(int i = 0; i < 255; i++)
-    {
-        float prevX = x;
-        float prevY = y;
-
-        vy += PROJECTILE_GRAVITY * dt;
-        x += vx * dt;
-        y += vy * dt;
-        int x1 = (int)prevX, x2 = (int)x, y1 = (int)prevY, y2 = (int)y;
-        SDL_SetRenderDrawBlendMode(pPlayer->pRenderer, SDL_BLENDMODE_BLEND);
-        if((i+1)%20 == 0) 
-            {
-                for (int i = 0; i < AMOUNT_OF_TILES_HORIZONTAL; i++)
-                {
-                    for (int j = 0; j < AMOUNT_OF_TILES_VERTICAL; j++)
-                    {
-                        SDL_Rect tileRect = getTileRect(pMap, i, j);
-                        if (isTileActive(pMap, i, j))
-                            if(SDL_IntersectRectAndLine(&tileRect, &x1, &y1, &x2, &y2)) return;
-                    }
-                }
-                drawCircle(pPlayer->pRenderer, (int)x, (int)y, rad, 255-i);
-            }
-    }
-}
-
 void enableTrigger(Player *pPlayer, int enable)
 {
     if(enable)
@@ -377,7 +283,7 @@ static void flipCanon(Player *pPlayer)
 void updatePlayer(Player *pPlayer, Map *pMap)
 {
     int mousePosx, mousePosy;
-    double diffAngle;
+    float diffAngle;
     Uint32 buttons = SDL_GetMouseState(&mousePosx, &mousePosy);
     SDL_Rect previousHitbox = pPlayer->hitbox;
 
@@ -395,8 +301,8 @@ void updatePlayer(Player *pPlayer, Map *pMap)
     
     checkForPlayerCollision(pPlayer, pMap);
 
-    double dx = mousePosx - pPlayer->x - (pPlayer->hullRect.w)/2;
-    double dy = mousePosy - pPlayer->y + (pPlayer->hullRect.h)/2;
+    float dx = mousePosx - pPlayer->x - (pPlayer->hullRect.w)/2;
+    float dy = mousePosy - pPlayer->y + (pPlayer->hullRect.h)/2;
 
     pPlayer->targetAngle = atan2(dy, dx);
 
