869753d748ba9a22c08b839f4f3aa06674a56925
Jonatanlvl <jonatan.vl@telia.com>
2026-05-11 13:36:45 +0200

    sound works

--- src/main.c
+++ src/main.c
@@ -37,6 +37,9 @@ void handleInput(Game *pGame, const Uint8 *keystate, bool *pInGameMenu);
 
 int main(int argc, char **argv)
 {
+    printf("main started\n");
+    fflush(stdout);
+   
     Game game = {0};
 
     if (!initiate(&game)) 
@@ -52,6 +55,9 @@ int main(int argc, char **argv)
 
 int initiate(Game *pGame)
 {
+    printf("initiate started\n");
+    fflush(stdout);
+
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) 
     {
         printf("SDL Init Error: %s\n", SDL_GetError());
@@ -65,13 +71,13 @@ int initiate(Game *pGame)
         return 0;
     }
 
-
     pGame->sounds = createSound();
     if (!pGame->sounds)
     {
         printf("Sound allocation failed\n");
         return 0;
     }
+    
     if (!initSound(pGame->sounds))
     {
         printf("Sound init failed\n");
